24e8d96ab9856ad8160c2004dd18da989e36213d
Júlio Gori Corradi <jgcorradi@gmail.com>
2021-02-26 20:58:59 -0300

    Update test.groovy

--- libraries/npm/steps/test.groovy
+++ libraries/npm/steps/test.groovy
@@ -1,7 +1,16 @@
 void call() {
-  stage('NPM: Test'){
-    docker.image("node:${node_version}").inside {
-      sh 'npm run test:all'
-    }
+  try {
+    networkId = UUID.randomUUID().toString()
+    sh "docker network create ${networkId}"
+    
+    stage('NPM: Test'){
+      docker.image("postgres:${config.postgres_version}").inside("--network ${networkId} --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres") {
+        docker.image("node:${config.node_version}").inside("--network ${networkId}") {
+          sh 'npm run test:all'
+        }
+      }
+    } 
+  } finally {
+    sh "docker network rm ${networkId}"
   }
 }
