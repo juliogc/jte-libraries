74c812c08c85267651c3a6b689ff1bbdd97b30fc
Júlio Gori Corradi <jgcorradi@gmail.com>
2021-02-26 21:31:11 -0300

    Update test.groovy

--- libraries/npm/steps/test.groovy
+++ libraries/npm/steps/test.groovy
@@ -1,16 +1,24 @@
 void call() {
   try {
     networkId = UUID.randomUUID().toString()
+    
+    // Creates network to be shared between service and database
     sh "docker network create ${networkId}"
     
+    // Override DATABASE_URL once we need dynamicaly update it
+    // between jobs executions to avoid container conflicts
+    postgresContainer = "postgres-${networkId}"
+    env.DATABASE_URL = "postgresql://postgres:postgres@${postgresContainer}:5432/postgres"
+    
     stage('NPM: Test'){
-      docker.image("postgres:${config.postgres_version}").withRun("--network ${networkId} --name postgres-${networkId} -p 5432:5432 -e POSTGRES_PASSWORD=postgres") {
+      docker.image("postgres:${config.postgres_version}").withRun("--network ${networkId} --name ${postgresContainer} -p 5432:5432 -e POSTGRES_PASSWORD=postgres") {
         docker.image("node:${config.node_version}").inside("--network ${networkId}") {
           sh 'npm run test:all'
         }
       }
     } 
   } finally {
+    // Deletes shared network
     sh "docker network rm ${networkId}"
   }
 }
